9c7ce8241ca98421d381a39a8374bbae50b22c62
Frank Sather <frank.sather@gmail.com>
2020-10-13 13:54:13 +0200

    update .gitignore and test scripts.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import time
 import asyncio
 import chess
@@ -34,13 +35,57 @@ result.move
 result.move.uci()
 board.push(result.move)
 
+board = chess.Board()
+info = engine.analyse(board, chess.engine.Limit(depth=16))
+info['score']
+info
+board.set_fen('rnbqkbnr/p2p2pp/2p5/4pp2/p7/NP2P1P1/P1PP1P1P/R1B1KBNR w KQkq - 0 8')
+board.is_stalemate()
+board.can_claim_draw()
+board.is_game_over(claim_draw=False)
+board.is_checkmate()
+board
+
+
+3928 % 73
+np.floor(3928/73)
+3693 % 73
+np.floor(3693/73)
+3397 % 73
+np.floor(3397/73)
+3461 % 73
+np.floor(3461/73)
+4360 % 73
+np.floor(4360/73)
+3189 % 73
+np.floor(3189/73)
+2893 % 73
+np.floor(2893/73)
+2237 % 73
+np.floor(2237/73)
+2020-10-08 04:41:58,152 INFO action with highest Q + U...3928
+2020-10-08 04:41:58,158 INFO PLAYER TURN...-1
+2020-10-08 04:41:58,158 INFO action with highest Q + U...3693
+2020-10-08 04:41:58,164 INFO PLAYER TURN...1
+2020-10-08 04:41:58,165 INFO action with highest Q + U...3397
+2020-10-08 04:41:58,171 INFO PLAYER TURN...-1
+2020-10-08 04:41:58,171 INFO action with highest Q + U...3461
+2020-10-08 04:41:58,177 INFO PLAYER TURN...1
+2020-10-08 04:41:58,177 INFO action with highest Q + U...4360
+2020-10-08 04:41:58,182 INFO PLAYER TURN...-1
+2020-10-08 04:41:58,183 INFO action with highest Q + U...3189
+2020-10-08 04:41:58,188 INFO PLAYER TURN...1
+2020-10-08 04:41:58,188 INFO action with highest Q + U...2893
+2020-10-08 04:41:58,194 INFO PLAYER TURN...-1
+2020-10-08 04:41:58,194 INFO action with highest Q + U...2237
+2020-10-08 04:41:58,200 INFO PLAYER TURN...1
+2020-10-08 04:41:58,200 INFO action with highest Q + U...3928
+
+
+
+
 # board = chess.Board()
 #
 # with chess.polyglot.open_reader("C:\WinBoard-4.8.0\WinBoard\default_book.bin") as reader:
 #     for entry in reader.find_all(board):
 #         print(entry.move, entry.weight, entry.learn)
-
-board = chess.Board()
-board.set_fen('8/5p1k/6pP/8/4P1qP/8/r4P2/1r3K2 w - - 7 49')
-board.is_stalemate()
-board
--- testrun.py
+++ testrun.py
@@ -30,6 +30,10 @@ import pickle
 
 import config
 import chess
+import chess.engine
+import time
+import multiprocessing
+from selfplay import _selfplay
 
 lg.logger_main.info('=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*')
 lg.logger_main.info('=*=*=*=*=*=.      NEW LOG      =*=*=*=*=*')
@@ -40,12 +44,17 @@ env = Game()
 env.gameState
 env.identities(env.gameState, env.gameState.board)
 
+t0 = time.perf_counter()
 
+t0
+t1 = time.perf_counter() - t0
+t1
 
 500 in env.gameState.allowedActions
 board = chess.Board()
 board
 
+engine = chess.engine.SimpleEngine.popen_uci("C:\WinBoard-4.8.0\Stockfish\stockfish_20090216_x64_bmi2.exe")
 
 board.can_claim_draw()
 board.is_check()
@@ -55,14 +64,38 @@ gstate.allowedActions
 best_player.get_preds(gstate)
 
 gstate.allowedActions
-board
-
+board.reset()
 
 board.fen()
 board.board_fen()
-board.set_board_fen('1p6/2P4P/8/8/8/8/2p5/8')
-board.set_fen('1p1p4/2P4P/8/8/8/8/2p5/1P1P4 b')
-board.set_fen('2r2k2/6b1/1p6/pn2p1R1/P3p2B/2NP3P/4Kn2/8 b - - 3 52')
+board.set_fen('6r1/5k2/8/n7/3K4/8/8/8 b - - 0 136')
+
+board.can_claim_draw()
+board.is_checkmate()
+board.can_claim_fifty_moves()
+board.is_insufficient_material()
+
+result = engine.play(board, chess.engine.Limit(time=0.01))
+result.ponder
+result.move
+info = engine.analyse(board,chess.engine.Limit(depth=8))
+info['score'].white()<chess.engine.Cp(-800)
+info['score'].relative<chess.engine.Cp(-800)
+info['score'].white()
+info['score'].black()<chess.engine.Cp(-800)
+info['score'].black()
+print(info['score'])
+board.can_claim_threefold_repetition()
+board.is_game_over(claim_draw=True)
+board
+
+statefen = board.fen()
+statefen = statefen.split(' ')
+id = '{0} {1}'.format(statefen[0],statefen[1])
+id
+board.can_claim_draw()
+board.can_claim_threefold_repetition()
+board.can_claim_fifty_moves()
 board.legal_moves
 lmvs = {}
 for move in board.legal_moves:
@@ -84,36 +117,104 @@ chess.piece_name(4)
 
 
 chessenv = Game()
-
 memory = Memory(config.MEMORY_SIZE)
 memory.commit_stmemory(env.identities,env.gameState,env.actionSpace)
 memory.stmemory
 
+
+pool = multiprocessing.Pool(2)
+out = zip(pool.map(_selfplay, range(0, 2)))
+t = tuple(out)
+
+len(t)
+
 chessenv.action_size
 chessenv.state_size
 chessenv.grid_shape
-
-current_NN = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, (12,)+chessenv.grid_shape, chessenv.action_size, config.HIDDEN_CNN_LAYERS)
-best_NN = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, (12,)+chessenv.grid_shape, chessenv.action_size, config.HIDDEN_CNN_LAYERS)
+current_NN = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, (119,)+chessenv.grid_shape, chessenv.action_size, config.HIDDEN_CNN_LAYERS)
+best_NN = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, (119,)+chessenv.grid_shape, chessenv.action_size, config.HIDDEN_CNN_LAYERS)
 best_NN.model.set_weights(current_NN.model.get_weights())
-best_player_version = 0
+
+best_player_version  = 2
+print('LOADING MODEL VERSION ' + str(2) + '...')
+m_tmp = best_NN.read(chessenv.name, 2, best_player_version)
+current_NN.model.set_weights(m_tmp.get_weights())
+best_NN.model.set_weights(m_tmp.get_weights())
+
 current_player = Agent('current_player', chessenv.state_size, chessenv.action_size, config.MCTS_SIMS, config.CPUCT, current_NN)
 best_player = Agent('best_player', chessenv.state_size, chessenv.action_size, config.MCTS_SIMS, config.CPUCT, best_NN)
+
 
 state = chessenv.reset()
 state.render(None)
 # action, pi, MCTS_value, NN_value = current_player.act(state, 0)
 
- _, memory, _, _ = playMatches(best_player, best_player, config.EPISODES, lg.logger_main, turns_until_tau0 = config.TURNS_UNTIL_TAU0, memory = memory)
+scores, memory, _, _ = playMatches(best_player, best_player, config.EPISODES, lg.logger_main, turns_until_tau0 = config.TURNS_UNTIL_TAU0, memory = memory)
 
+current_player.replay(memory.ltmemory)
+
+current_NN.write(chessenv.name, 3)
+
+ltmem = memory.ltmemory
+memory_samp = random.sample(memory.ltmemory, min(1000, len(memory.ltmemory)))
+len(memory_samp)
+for s in memory_samp:
+    current_value, current_probs, _ = current_player.get_preds(s['state'])
+    best_value, best_probs, _ = best_player.get_preds(s['state'])
+
+    lg.logger_memory.info('MCTS VALUE FOR %s: %f', s['playerTurn'], s['value'])
+    lg.logger_memory.info('CUR PRED VALUE FOR %s: %f', s['playerTurn'], current_value)
+    lg.logger_memory.info('BES PRED VALUE FOR %s: %f', s['playerTurn'], best_value)
+    lg.logger_memory.info('THE MCTS ACTION VALUES: %s', ['%.2f' % elem for elem in s['AV']]  )
+    lg.logger_memory.info('CUR PRED ACTION VALUES: %s', ['%.2f' % elem for elem in  current_probs])
+    lg.logger_memory.info('BES PRED ACTION VALUES: %s', ['%.2f' % elem for elem in  best_probs])
+    lg.logger_memory.info('ID: %s', s['state'].id)
+    # lg.logger_memory.info('INPUT TO MODEL: %s', current_player.model.convertToModelInput(s['state']))
+
+    s['state'].render(lg.logger_memory)
 
 len(memory.ltmemory)
-np.sum((memory.ltmemory[1665]['AV'])>0)
-pickle.dump( memory, open("run/memory/memory_chess" + ".p", "wb" ) )
+memory = None
+scores
+scores, _, points, sp_scores = playMatches(best_player, current_player, config.EVAL_EPISODES, lg.logger_tourney, turns_until_tau0 = 0, memory = None)
+best_player_version = 2
+best_NN.model.set_weights(current_NN.model.get_weights())
 
-mem2 = pickle.load(open("run/memory/memory_chess" + ".p",   "rb" ) )
-len(mem2.ltmemory)
-minibatch = random.sample(mem2.ltmemory, min(config.BATCH_SIZE, len(mem2.ltmemory)))
+
+
+actions = np.argwhere(memory.ltmemory[8]['AV'] == max(memory.ltmemory[8]['AV']))
+actions = np.random.multinomial(1,memory.ltmemory[8]['AV'])
+actions
+np.where(actions==1)
+
+random.choice(actions)[0]
+memory.ltmemory[8]['AV'][370:390]
+memory.ltmemory[0]['board']=None
+memory.ltmemory['state'].engine=None
+memory.clear_stmemory()best_player_version = best_player_version + 1
+best_NN.model.set_weights(current_NN.model.get_weights())
+len(memory.ltmemory)
+
+
+pickle.dump(memory, open( run_folder + "memory/memory" + str(4).zfill(4) + ".p", "wb" ) )
+
+memory.ltmemory[1]
+mem = Memory(config.MEMORY_SIZE)
+
+for m in memory.ltmemory:
+	mem.commit_stmemory(m)
+
+memory = pickle.load(open( run_folder + "memory/multiproc/memory" + str(2).zfill(4) + ".p",   "rb" ) )
+len(memory[0].ltmemory)
+memory[0].ltmemory[32]
+memory[0].ltmemory[32]['state'].movetable
+memory[0].ltmemory[32]['state'].board
+
+sum(memory.ltmemory[9]['AV']>0)
+memory.ltmemory[9]['state'].movetable
+memory = mem2
+
+minibatch = random.sample(memory.ltmemory, min(config.BATCH_SIZE, len(memory.ltmemory)))
 len(minibatch)
 training_states = np.array([current_NN.convertToModelInput(row['state']) for row in minibatch])
 training_targets = {'value_head': np.array([row['value'] for row in minibatch])
@@ -246,3 +347,6 @@ hm = board.pieces(1,1)
 type(hm)
 for move in board.legal_moves:
 	print(board.san(move))
+
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
